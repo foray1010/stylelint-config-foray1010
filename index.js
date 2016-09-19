e1d04b054fbb8bfdcb0734b8f91d5255191b58f7
foray1010 <foray1010@users.noreply.github.com>
2016-09-19 23:47:51 +0800

    Disable at-rule-no-unknown

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-css-modules'
-  ]
+  ],
+  rules: {
+    // disable this rule because it is not compatible with SCSS
+    'at-rule-no-unknown': null
+  }
 }
