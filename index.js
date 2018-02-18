c8241562bbf60c6b9c048d4b8ab7fa3f0191b90a
foray1010 <foray1010@users.noreply.github.com>
2018-02-19 00:11:34 +0800

    4.0.1: disable no-descending-specificity

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
   rules: {
     // not compatible with SCSS
     'at-rule-no-unknown': null,
+    // doesn't work with cssnext mixins
+    'no-descending-specificity': null,
     // if used with styled-component, we can expect there are some js files without any style
     'no-empty-source': null
   }
